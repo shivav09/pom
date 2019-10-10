29759383390be6ca93b397bc5a8d5f32cda5b683
Vallabhaneni <Vallabhaneni@Vallabhaneni-PC>
2019-10-10 22:07:07 +0530

    Added selenium basics

---
+++ pom/src/main/java/Utils/SeleniumUtils.java
@@ -0,0 +1,127 @@
+package Utils;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumUtils {
+
+	WebDriver driver;
+	Actions actions;
+
+	public SeleniumUtils(WebDriver dvr) {
+		driver = dvr;
+		actions = new Actions(driver);
+	}
+
+	public void pageLoadTime(long time) {
+		driver.manage().timeouts().pageLoadTimeout(time, TimeUnit.SECONDS);
+	}
+
+	public WebElement findByXpath(String path) {
+		return driver.findElement(By.xpath(path));
+	}
+
+	public List<WebElement> findElementsByXpath(String path) {
+		return driver.findElements(By.xpath(path));
+	}
+
+	public void waitForElement(int sec) {
+		try {
+			Thread.sleep(sec);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void mouseHoverAndClick(WebElement moveTo, WebElement clickElement) {
+		actions.moveToElement(moveTo).click(clickElement).build().perform();
+	}
+
+	public void mouseHoverOnElement(WebElement element) {
+		actions.moveToElement(element).build().perform();
+	}
+
+	public void waitForLinkTobePresent(String expression) {
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText(expression)));
+
+	}
+
+	public void waitForXpathPresent(String path) {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(path)));
+	}
+
+	public void waitForElementToClickable(String path) {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(path)));
+
+	}
+
+	public void selectByText(String dropdownPath, String text) {
+		Select select = new Select(driver.findElement(By.xpath(dropdownPath)));
+		select.selectByValue(text);
+	}
+
+	public void selectByIndex(String dropDownPath, int index) {
+		Select select = new Select(driver.findElement(By.xpath(dropDownPath)));
+		select.selectByIndex(index);
+	}
+
+	public void selectByVisibleText(String dropDownPath, String text) {
+		Select select = new Select(driver.findElement(By.xpath(dropDownPath)));
+		select.selectByVisibleText(text);
+	}
+
+	public void clickOnTabKey() {
+		actions.sendKeys(Keys.TAB).perform();
+	}
+
+	public void switchToFrame(String frameName) {
+		driver.switchTo().frame(frameName);
+	}
+
+	public void swithToDefault() {
+		driver.switchTo().defaultContent();
+	}
+
+	public void highlightElement(WebElement element) {
+		JavascriptExecutor javascriptExecutor = ((JavascriptExecutor) driver);
+		javascriptExecutor.executeScript("arguments[0].style.border='2px solid yellow'", element);
+	}
+
+	public void scrollDownJavaScript() {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+	}
+
+	public void scrollUpJavaScript() {
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("window.scrollTo(document.body.scrollHeight, 0)");
+
+	}
+
+	public void scrollByXYPosition(String Xposition, String Yposition) {
+		JavascriptExecutor javascriptExecutor = ((JavascriptExecutor) driver);
+		javascriptExecutor.executeScript("window.scrollTo(" + Xposition + "," + Yposition + ")");
+	}
+
+	public void scrollToElement(WebElement webElement) {
+		JavascriptExecutor javascriptExecutor = ((JavascriptExecutor) driver);
+		javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", webElement);
+	}
+
+}
--- pom/src/main/java/baseClass/BaseClass.java
+++ pom/src/main/java/baseClass/BaseClass.java
@@ -1,17 +1,13 @@
 package baseClass;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import javax.swing.plaf.FileChooserUI;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -19,46 +15,52 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseClass {
+import Utils.SeleniumUtils;
+
+public class BaseClass extends SeleniumUtils {
 
 	public static WebDriver driver;
 	public static Properties prop;
 	public static FileInputStream fis;
-	public BaseClass(){
-		 try {
-			fis =new FileInputStream("C://Users//Vallabhaneni//git//pom//pom//resources//config.properties");
-			
+
+	public BaseClass() {
+		super(driver);
+		try {
+			fis = new FileInputStream("C://Users//Vallabhaneni//git//pom//pom//resources//config.properties");
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		 prop = new Properties();
-		 try {
+		prop = new Properties();
+		try {
 			prop.load(fis);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public static void initialize(){
+
+	public static void initialize() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
 	}
-	
+
 	public static String timestamp() {
-	    return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
-	} 
-	public void takeScreenshot(String name){
-		 
-		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
+	}
+
+	public void takeScreenshot(String name) {
+
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		try {
-			FileUtils.copyFile(src,new File("C://Users//Vallabhaneni//git//pom//pom//test-output//Screenshots//"+name+"_"+timestamp()+".png"));
+			FileUtils.copyFile(src, new File("C://Users//Vallabhaneni//git//pom//pom//test-output//Screenshots//" + name
+					+ "_" + timestamp() + ".png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
